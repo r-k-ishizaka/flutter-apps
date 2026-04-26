631ec39105cbac88ea8d67ad2bb9178007033889
r-kikuchi-i <mayo.mayo.panic+git@gmail.com>
2026-04-26 21:31:54 +0900

    DefaultScaffold を使うように修正

--- apps/light_key/lib/screens/auth/auth_screen.dart
+++ apps/light_key/lib/screens/auth/auth_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:design_system/design_system.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:provider/provider.dart';
@@ -69,7 +70,7 @@ class AuthScreen extends HookWidget {
       };
     }, []);
 
-    return Scaffold(
+    return DefaultScaffold(
       appBar: AppBar(
         title: const Text('ログイン'),
       ),
